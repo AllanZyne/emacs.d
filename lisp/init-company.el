db60e76ea2746f65a2a745ff6735310ed11b6f59
Allan Zyne <allanzyne@outlook.com>
2017-01-17 09:50:45 +0800

    + company filter

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,7 +1,8 @@
 
 (use-package company
   :config
-  (global-company-mode 1))
+  (global-company-mode 1)
+  (bind-key* "C-s" 'company-filter-candidates company-active-map))
 
 
 (provide 'init-company)
