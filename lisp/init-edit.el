3513dd92fd6c82fadb409aee5171e87251f136c3
Allan Zyne <allanzyne@outlook.com>
2017-01-19 09:00:25 +0800

    - bind-key

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -15,6 +15,17 @@
 
 (define-key key-translation-map [?\C-h] [?\C-?])
 
+;; comment
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+(bind-key* "C-;" 'comment-or-uncomment-region-or-line)
 
 ;; move lines
 (use-package drag-stuff
--- lisp/init-prelude.el
+++ lisp/init-prelude.el
@@ -26,17 +26,4 @@
 (set-frame-size-according-to-resolution)
 
 
-;; comment
-(defun comment-or-uncomment-region-or-line ()
-    "Comments or uncomments the region or the current line if there's no active region."
-    (interactive)
-    (let (beg end)
-        (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-            (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end)))
-
-(bind-key* "C-;" 'comment-or-uncomment-region-or-line)
-
-
 (provide 'init-prelude)
