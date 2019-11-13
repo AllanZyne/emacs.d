33951a84bf26f3ce38203359b671e3ad3fdc1889
AllanZyne <allanzyne@outlook.com>
2019-11-13 18:50:25 +0800

    * coq

--- lisp/init-coq.el
+++ lisp/init-coq.el
@@ -5,10 +5,21 @@
 (require-package 'proof-general)
 
 (after-load 'coq-mode
+  ;; (unicode-tokens-mode -1)
   (setq coq-compile-before-require t)
+  (define-key coq-mode-map (kbd "M-<up>") 'proof-undo-last-successful-command)
   (define-key coq-mode-map (kbd "M-<down>") 'proof-assert-next-command-interactive)
-  (define-key coq-mode-map (kbd "M-<right>") 'proof-goto-point))
+  (define-key coq-mode-map (kbd "M-<right>") 'proof-goto-point)
+  (define-key coq-mode-map (kbd "M-p") 'coq-Print)
+  (define-key coq-mode-map (kbd "M-c") 'coq-Check)
+  (define-key coq-mode-map (kbd "M-q") 'coq-query)
+  (define-key coq-mode-map (kbd "M-l") 'coq-LocateNotation))
 
+(require-package 'company-coq)
+
+(after-load 'company-coq
+  (add-hook 'coq-mode-hook #'company-coq-mode)
+  (setq company-coq-disabled-features '(hello)))
 
 (provide 'init-coq)
 ;;; init-csv.el ends here
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -49,20 +49,20 @@
 ;;------------------------------------------------------------------------------
 ;; Font
 ;;------------------------------------------------------------------------------
-(set-frame-font "-unknown-Fira Code-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1")
+(set-frame-font "Fira Code 12")
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
-                    charset (font-spec :family "Microsoft Yahei" :size 16)))
+                    charset (font-spec :family "Noto Sans CJK SC" :size 16)))
 
 (add-to-list 'after-make-frame-functions
 	     (lambda (new-frame)
 	       (select-frame new-frame)
                ;; English Font
-               (set-face-attribute 'default nil :font "Fira Code 13")
+               (set-face-attribute 'default nil :font "Fira Code 12")
                ;; Chinese Font
                (dolist (charset '(kana han symbol cjk-misc bopomofo))
                  (set-fontset-font (frame-parameter nil 'font)
-                                   charset (font-spec :family "Microsoft Yahei" :size 16)))))
+                                   charset (font-spec :family "Noto Sans CJK SC" :size 16)))))
 
 
 (provide 'init-themes)
