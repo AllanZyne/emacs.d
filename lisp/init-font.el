14a2f6edc3543c207f2240b5813d7c62bf40619d
Allan Zyne <allanzyne@outlook.com>
2019-12-22 11:44:44 +0800

    + fira code

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -2,20 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (fboundp 'global-prettify-symbols-mode)
-  (add-hook 'after-init-hook 'global-prettify-symbols-mode))
 
 ;; (set-frame-font "Source Code Pro 12")
 (set-frame-font "Fira Code 12")
-(require 'fira-code-mode)
-
-(add-to-list 'after-make-frame-functions
-             (lambda (new-frame)
-               (select-frame new-frame)
-               (set-face-attribute 'default nil :font "Fira Code 12")
-               (fira-code-mode)))
-
-(add-hook 'prettify-symbols-mode-hook 'fira-code-mode)
 
 ;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
 ;;   (set-fontset-font (frame-parameter nil 'font)
@@ -30,6 +19,16 @@
 ;;                (dolist (charset '(kana han symbol cjk-misc bopomofo))
 ;;                  (set-fontset-font (frame-parameter nil 'font)
 ;;                                    charset (font-spec :family "Noto Sans CJK SC" :size 16)))))
+
+;;; Prettify Symbols
+(require 'fira-code-mode)
+(diminish 'fire-code-mode)
+
+(when (fboundp 'global-prettify-symbols-mode)
+  (add-hook 'after-init-hook 'global-prettify-symbols-mode))
+
+(add-hook 'prettify-symbols-mode-hook 'fira-code-mode)
+
 
 (provide 'init-font)
 ;;; init-font.el ends here
