29dee252b85721a52ff692f2c5d543959f817c53
Allan Zyne <allanzyne@outlook.com>
2019-12-22 11:45:11 +0800

    + coq: more shortcuts

--- lisp/init-coq.el
+++ lisp/init-coq.el
@@ -4,6 +4,7 @@
 
 (require-package 'proof-general)
 (require-package 'company-coq)
+(diminish 'company-coq-mode)
 
 (after-load 'coq-mode
   (add-hook 'coq-mode-hook 'company-coq-mode)
@@ -13,6 +14,11 @@
   (define-key coq-mode-map (kbd "M-<up>") 'proof-undo-last-successful-command)
   (define-key coq-mode-map (kbd "M-<down>") 'proof-assert-next-command-interactive)
   (define-key coq-mode-map (kbd "M-<right>") 'proof-goto-point)
+
+  (define-key coq-mode-map (kbd "M-p") 'proof-undo-last-successful-command)
+  (define-key coq-mode-map (kbd "M-n") 'proof-assert-next-command-interactive)
+  (define-key coq-mode-map (kbd "M-l") 'proof-goto-point)
+
   (define-key coq-mode-map (kbd "C-M-p") 'coq-Print)
   (define-key coq-mode-map (kbd "C-M-c") 'coq-Check)
   (define-key coq-mode-map (kbd "C-M-k") 'coq-SearchAbout)
@@ -22,7 +28,6 @@
   (set-fontset-font t 'greek (font-spec :name "DejaVu sans Mono-13:bold") nil 'prepend))
 
 (after-load 'company-coq-mode
-  (diminish 'company-coq-mode)
   (setq company-coq-disabled-features '(hello keybindings prettify-symbols)))
 
 (provide 'init-coq)
